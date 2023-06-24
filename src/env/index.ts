700c0f37e2658a220af0a36b589a1501434d228a
luanrem <luanrem@gmail.com>
2023-06-23 23:05:11 -0300

    feat: create db with docker

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,9 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
+  POSTGRESQL_USERNAME: z.string(),
+  POSTGRESQL_PASSWORD: z.string(),
+  POSTGRESQL_DATABASE: z.string().default('friendapi'),
 })
 
 const _env = envSchema.safeParse(process.env)
